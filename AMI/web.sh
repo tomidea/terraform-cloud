03454e81cd1f1751a8abf81196ddc61c7b769ce9
tomidea <adedeji.tomide@yahoo.com>
2022-06-28 19:19:05 +0100

    plan modification-2

--- AMI/web.sh
+++ AMI/web.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo yum install -y dnf-utils http://rpms.remirepo.net/enterprise/remi-release-8.rpm
-sudo yum install -y mysql wget vim telnet htop git python3 net-tools 
+sudo yum install -y mysql wget vim telnet htop git python3 net-tools zip
 sudo systemctl start chronyd
 sudo systemctl enable chronyd
 sudo yum module reset php -y

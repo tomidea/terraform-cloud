f7152a288991558c7732078143c45312816d3179
tomidea <adedeji.tomide@yahoo.com>
2022-06-29 18:40:30 +0100

    update provider

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
   region                  = var.region
-  shared_credentials_files = ["~/.aws/credentials"]
-  profile                 = "terraform-aws"
+  #shared_credentials_files = ["~/.aws/credentials"]
+ # profile                 = "terraform-aws"
 }

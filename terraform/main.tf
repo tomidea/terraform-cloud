9a2711fabed1b16857e37c52465b796375a35815
tomidea <adedeji.tomide@yahoo.com>
2022-07-02 18:29:11 +0100

    ASG updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -68,9 +68,9 @@ module "AutoScaling" {
   ami-web           = var.ami-web
   ami-bastion       = var.ami-bastion
   ami-nginx         = var.ami-nginx
-  desired_capacity  = 2
-  min_size          = 2
-  max_size          = 2
+  desired_capacity  = 1
+  min_size          = 1
+  max_size          = 1
   bastion-asg-name  = "bastion-asg"
   web-sg            = [module.security.web-sg]
   bastion-sg        = [module.security.bastion-sg]
